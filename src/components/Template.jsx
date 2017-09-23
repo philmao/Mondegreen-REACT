82e3d2a4cd7733645a39b63cb87722cbfa500e66
TannerGruebele <tanner@saambaa.com>
2017-09-23 12:33:25 -0700

    changed / route

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HeaderContainer from './shared/HeaderContainer';
-import HomePage from './home/HomePageContainer';
 import LoginPage from './account/LoginPageContainer';
 import ProfilePage from './account/ProfilePage';
 import RegisterPage from './account/RegisterPageContainer';
@@ -15,7 +14,7 @@ export default function Template(props) {
       <div className="wrapper">
         <HeaderContainer authentication={authentication} />
         <section className="page-content container-fluid">
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={ProfilePage} />
           <Route exact path="/account/login" component={LoginPage} />
           <Route exact path="/account/register" component={RegisterPage} />
           <Route path="/account/profile/:id" component={ProfilePage} />
