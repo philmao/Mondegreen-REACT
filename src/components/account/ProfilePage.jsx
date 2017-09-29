3f65f0741956f1a35668049b3e9e94867a469495
Kira Zoltan <kira@Kiras-MacBook-Pro.local>
2017-09-29 15:29:37 -0700

    more style changes to landing page

--- src/components/account/ProfilePage.jsx
+++ src/components/account/ProfilePage.jsx
@@ -12,9 +12,13 @@ export default function ProfilePage(props) {
     <section className="page-content">
       <div className="container">
         <div className="row">
-          <Video>
-            <Button color="primary" size="lg" block tag={Link} to={`/game_console/${match.params.id}`}>Start Game</Button>
+        
+          <Button id="start" color="secondary" size="lg" tag={Link} to={`/game_console/${match.params.id}`}>Start Playing!</Button>
+        
+        <Video>
+
           </Video>
+
         </div>
       </div>
     </section>
--- src/components/account/VideoComponent.jsx
+++ src/components/account/VideoComponent.jsx
@@ -2,12 +2,13 @@
 
 import React, { Component } from 'react';
 
+
 class Video extends Component {
     constructor (props) {
         super(props);
 
         this.state = {
-            videoURL: 'https://15-lvl3-pdl.vimeocdn.com/01/4246/4/121231934/342466861.mp4?expires=1506661749&token=0724c6beb6d3ebf873bde'
+            videoURL: 'http://philipmao.com/video/632333283.mp4'
         }
     }
 
@@ -16,6 +17,7 @@ class Video extends Component {
             <video id="background-video" loop autoPlay>
                 <source src={this.state.videoURL} type="video/mp4" />
                 <source src={this.state.videoURL} type="video/ogg" />
+               
                 Your browser does not support the video tag.
             </video>
         )
