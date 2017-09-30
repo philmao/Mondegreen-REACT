9c8fbe2930422f0d1847d8538a21255b575743f5
Kira Zoltan <kira@Kiras-MacBook-Pro.local>
2017-09-29 19:44:26 -0700

    added jumbotron and more styling to landing page

--- src/components/account/ProfilePage.jsx
+++ src/components/account/ProfilePage.jsx
@@ -1,6 +1,6 @@
 
 
-import { Button } from 'reactstrap';
+import { Button, Jumbotron, Navbar, NavbarBrand } from 'reactstrap';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Video from './VideoComponent';
@@ -9,21 +9,27 @@ export default function ProfilePage(props) {
   const { match } = props;
 
   return (
+    
     <section className="page-content">
+  
       <div className="container">
         <div className="row">
+        <Jumbotron>
+        <h1 className="display-3">Mondegreen</h1>
+        <p> /ˈmɒndɪɡriːn/ is a mishearing or misinterpretation of a phrase as a result of near-homophony, in a way that gives it a new meaning.</p>
+        <hr />
+        
+      </Jumbotron>
         
           <Button id="start" color="secondary" size="lg" tag={Link} to={`/game_console/${match.params.id}`}>Start Playing!</Button>
-        
-        <Video>
-
-          </Video>
-
+          <Video />
         </div>
       </div>
     </section>
   );
 }
+
+
 
 
 
